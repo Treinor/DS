6f6c77a32b27dd365876c18e7086a72d73592208
Treinor <37077094+Treinor@users.noreply.github.com>
2020-02-28 00:35:54 -0500

    esta es la buena

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
@@ -24,8 +25,10 @@ var unidad_organica = require('./routes/unidad_organica')
 var codigo = require('./routes/codigo')
 var codigob = require('./routes/codigob')
 //middlewares
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client')));
+app.use('/', express.static('client', { redirect: false }))
 
 //cors
 // Configuracion para intercambio de recursos de origenes cruzados CORS //
@@ -34,30 +37,33 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
- 
+
     next();
 });
 
 
 //rutas
-app.use ('/api', accion_personal_routes);
-app.use ('/api', datosExternos);
-app.use ('/api', autoridad_nominadora_routes);
-app.use ('/api', canton);
-app.use ('/api', denominacion_puesto);
-app.use ('/api', escala_ocupacional);
-app.use ('/api', funcionario);
-app.use ('/api', modalidad_laboral);
-app.use ('/api', nivel_ocupacional);
-app.use ('/api', personal);
-app.use ('/api', provincia);
-app.use ('/api', puesto_adicional);
-app.use ('/api', recursos_humanos);
-app.use ('/api', regimen_laboral);
-app.use ('/api', responsable_registro);
-app.use ('/api', unidad_organica);
-app.use ('/api', codigo);
-app.use ('/api', codigob);
+app.use('/api', accion_personal_routes);
+app.use('/api', datosExternos);
+app.use('/api', autoridad_nominadora_routes);
+app.use('/api', canton);
+app.use('/api', denominacion_puesto);
+app.use('/api', escala_ocupacional);
+app.use('/api', funcionario);
+app.use('/api', modalidad_laboral);
+app.use('/api', nivel_ocupacional);
+app.use('/api', personal);
+app.use('/api', provincia);
+app.use('/api', puesto_adicional);
+app.use('/api', recursos_humanos);
+app.use('/api', regimen_laboral);
+app.use('/api', responsable_registro);
+app.use('/api', unidad_organica);
+app.use('/api', codigo);
+app.use('/api', codigob);
 //exportar
+app.get('*', function(req, res, next) {
+    res.sendFile(path.resolve('client/index.html'));
+});
 module.exports = app;
 
